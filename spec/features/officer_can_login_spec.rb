b5c232a83668bd70c73263c93612227b77eafa97
CJ Avilla <cjavilla@gmail.com>
2014-12-10 15:28:37 -0800

    Officer can sign in successfully

--- spec/features/officer_can_login_spec.rb
+++ spec/features/officer_can_login_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.feature 'Officer can sign in' do
   scenario 'successfully' do
     visit '/officers/sign_in'
-    fill_in 'email', with: 'jobs@apple.com'
-    fill_in 'password', with: 'password'
-    click 'Sign In'
+    fill_in 'Email', with: 'jobs@apple.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
   end
 end
