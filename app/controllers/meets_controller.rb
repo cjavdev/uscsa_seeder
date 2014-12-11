7635f76088c2687c4ff1342e01c08b5cfc98ef0f
CJ Avilla <cjavilla@gmail.com>
2014-12-11 09:04:13 -0800

    add controllers and some basic routes

---
+++ app/controllers/meets_controller.rb
@@ -0,0 +1,7 @@
+class MeetsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'events#index'
   devise_for :users
+
+  resources :meets do
+    resources :events
+  end
+  resources :athletes
+  resources :shchools
 end
