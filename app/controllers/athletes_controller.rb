9395a812fc870490c929b8e87dd6fc9c49915822
CJ Avilla <cjavilla@gmail.com>
2014-12-11 10:23:29 -0800

    white background was intense with the correct theme

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -1,4 +1,6 @@
 class AthletesController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
     @athletes = Athlete.all
   end
--- app/controllers/meets_controller.rb
+++ app/controllers/meets_controller.rb
@@ -1,4 +1,6 @@
 class MeetsController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
   end
 
