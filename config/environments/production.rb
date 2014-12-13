984e54cff5161efae9f152e4dc7df05c6073ba82
CJ Avilla <cjavilla@gmail.com>
2014-12-12 17:54:39 -0800

    stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.delivery_method ||= :smtp
   config.action_mailer.default_url_options = { host: 'skiseeder.herokuapp.com' }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
