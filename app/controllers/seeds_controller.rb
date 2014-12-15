50970f53c928c19f0813201b17b81645bcf4396b
Daniel <BurtDaniel@Gmail.com>
2014-12-15 10:55:18 -0800

    actually fixed proper athletes showing

--- app/controllers/seeds_controller.rb
+++ app/controllers/seeds_controller.rb
@@ -3,9 +3,9 @@ class SeedsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     if current_user.captain?
-      @team = current_user.athlete.school.teams.where({ discipline: @event.discipline, sex: @event.sex }).first
+      @team = current_user.athlete.school.teams.where({ sex: Team.sexes[@event.sex], discipline: Team.disciplines[@event.discipline] }).first
     else
-      @teams = Team.all.includes(:athletes, :school).where({ discipline: @event.discipline, sex: @event.sex })
+      @teams = Team.all.includes(:athletes, :school).where({ sex: Team.sexes[@event.sex], discipline: Team.disciplines[@event.discipline] })
     end    
     render :show
   end
