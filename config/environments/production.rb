5b7e4284a1466f0b20587ee02dbc7f8a6385268f
CJ Avilla <cjavilla@gmail.com>
2014-12-20 19:53:50 -0800

    debugging mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'seeding.sccsc.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
@@ -69,8 +70,6 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-  ActionMailer::Base.delivery_method ||= :smtp
-  config.action_mailer.default_url_options = { host: 'skiseeder.herokuapp.com' }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
