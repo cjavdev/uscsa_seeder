13ffffa3084f5f456d5ba937d57a6e235c7ca011
Daniel <BurtDaniel@Gmail.com>
2014-12-21 14:51:44 -0800

    only allow seeding before registration closes

--- app/models/seed.rb
+++ app/models/seed.rb
@@ -17,12 +17,18 @@ class Seed < ActiveRecord::Base
   validates :athlete, :event, :seeded, presence: true
   validates :athlete_id, uniqueness: { scope: :event_id }
 
-  validate :valid_seed_num?
+  validate :valid_seed_num?, :registration_open
 
   def valid_seed_num?
     if ~self.seeded &&
         self.event.seeded_positions(self.athlete.school.id).include?(self.seeded)
       errors.add(:seeded, "Already a racer in that position")
+    end
+  end
+  
+  def registration_open
+    if !current_user.can_manage_league? && self.event.seeding_closes_at < Date.current
+      errors.add(:event, "Event is closed. Seeding can not be changed");
     end
   end
 end
